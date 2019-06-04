39f8cfd7e11f0d03aa37b410ff218510358f3590
Zac Halbert <zhalbert@metromile.com>
2019-06-03 22:54:22 -0700

    Add js for bg and overlay switching

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,27 @@
 import './modules'
 
-console.log(`app.js has loaded!`)
+// 
+
+$('.bg-option-1, .ol-option-1').show();
+
+$('.bg-switcher').click(function() {
+  const classToShow = $(this).data('switcher');
+  const toShow = $( '.'+classToShow );
+
+  $('.bg-switcher').removeClass('active');
+  $(this).addClass('active');
+  
+  $('.hero__bg-option').hide();
+  toShow.show();
+});
+
+$('.ol-switcher').click(function() {
+  const classToShow = $(this).data('switcher');
+  const toShow = $( '.'+classToShow );
+
+  $('.ol-switcher').removeClass('active');
+  $(this).addClass('active');
+  
+  $('.hero__ol-option').hide();
+  toShow.show();
+});
