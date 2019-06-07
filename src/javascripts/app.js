c68450a27aa30b8a068737e7d8cfd0631ea73e7a
Zac Halbert <zhalbert@metromile.com>
2019-06-07 09:32:03 -0700

    Much cleanup

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,20 @@
 import './modules'
 
-// 
+// Smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 $('.bg-option-2, .ol-option-1').show();
 
